038ed592cd9c42c8725f4e69fc896bcb439f8b85
John Kuefler <john.kuefler@hotmail.com>
2018-10-15 21:20:18 -0500

    added main page layout

--- KueflerBioApp/KueflerBioApp/App.xaml.cs
+++ KueflerBioApp/KueflerBioApp/App.xaml.cs
@@ -11,7 +11,7 @@ namespace KueflerBioApp
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
